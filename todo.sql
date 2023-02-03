b89a44cc7ea4187acfb921a582ef1323f7376ab4
Zeroflash1 <nigamrai2020@gmail.com>
2023-02-03 06:40:43 +0545

    database updated

--- todo.sql
+++ todo.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 21, 2023 at 01:53 PM
--- Server version: 10.4.25-MariaDB
--- PHP Version: 7.4.30
+-- Generation Time: Feb 03, 2023 at 01:55 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -31,9 +31,17 @@ CREATE TABLE `tbl_task` (
   `id` int(11) NOT NULL,
   `task` text NOT NULL,
   `date` date NOT NULL,
-  `completed` tinyint(1) NOT NULL,
+  `status` varchar(255) NOT NULL,
   `updated_date` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbl_task`
+--
+
+INSERT INTO `tbl_task` (`id`, `task`, `date`, `status`, `updated_date`) VALUES
+(2, 'Take Class of Web Development', '2023-02-13', 'Done', '0000-00-00'),
+(5, 'Take Class  fdafsd', '2023-02-13', 'Undone', '0000-00-00');
 
 -- --------------------------------------------------------
 
@@ -46,7 +54,15 @@ CREATE TABLE `tbl_user` (
   `username` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbl_user`
+--
+
+INSERT INTO `tbl_user` (`id`, `username`, `email`, `password`) VALUES
+(1, 'Nigam Rai', 'nigam@gmail.com', 'nigam'),
+(3, 'Chhiring Sherpa', 'chiring@gmail.com', 'chiring');
 
 --
 -- Indexes for dumped tables
@@ -72,13 +88,13 @@ ALTER TABLE `tbl_user`
 -- AUTO_INCREMENT for table `tbl_task`
 --
 ALTER TABLE `tbl_task`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `tbl_user`
 --
 ALTER TABLE `tbl_user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
